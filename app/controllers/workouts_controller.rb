0a8d956ae029998fe690fddc51b01ec4a8b13d77
MyMac <mymac@JCB.local>
2018-05-12 09:37:31 -0700

    added workout_params and find_workout to workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,6 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -19,4 +21,15 @@ class WorkoutsController < ApplicationController
 
   def destroy
   end
+
+
+ private 
+
+    def workout_params 
+      params.require(:workout).permit(:date, :comments)
+    end 
+
+    def find_workout 
+      @workout = workout.find_by(id: params[:id])
+    end 
 end
