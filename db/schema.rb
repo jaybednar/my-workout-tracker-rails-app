d55fffc784d61ae14981993971b7f9e14c575efa
MyMac <mymac@JCB.local>
2018-05-12 09:41:24 -0700

    removed url column from exercises

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_11_211321) do
+ActiveRecord::Schema.define(version: 2018_05_12_164003) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.string "bodypart"
     t.integer "reps"
     t.integer "sets"
-    t.string "url"
     t.integer "workout_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
