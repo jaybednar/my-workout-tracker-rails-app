0a0d0b7ee2e2297f49bd34b7049dbaf6cc68a759
MyMac <mymac@JCB.local>
2018-05-14 08:14:33 -0700

    moved logged_in? and current_user to application_helper.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
 
 
-	def logged_in?
-		!!session[:user_id]
-	end 
 
-	def current_user 
-		@user ||= User.find(session[:user_id])
-	end 
 
 	
 end
