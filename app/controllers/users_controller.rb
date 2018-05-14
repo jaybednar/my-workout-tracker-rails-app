3bfe2beddb7f869b960114d1fd0ddd40ae695311
MyMac <mymac@JCB.local>
2018-05-14 09:25:21 -0700

    added session login in user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if confirm_password 
         if @user.save
+          session[:user_id] = @user.id 
           redirect_to user_path(@user)
         else 
           render :new
