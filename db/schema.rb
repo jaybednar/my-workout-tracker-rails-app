ce670264d4c3b33961d99446e2361e948e9ea375
MyMac <mymac@JCB.local>
2018-05-14 15:46:04 -0700

    added uid column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_12_164003) do
+ActiveRecord::Schema.define(version: 2018_05_14_224518) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_05_12_164003) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid", limit: 25
   end
 
   create_table "workouts", force: :cascade do |t|
