f925ec4f34833b927adbb7e3aebbe1e18156959f
MyMac <mymac@JCB.local>
2018-05-14 16:04:00 -0700

    added User.find_or_create_by_omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	raise params.inspect
-  	@user = User.find_by(email: params[:email])
-  	if @user && @user.authenticate(params[:password])
-  		session[:user_id] = @user.id 
-  		redirect_to user_path(@user)
-  	else
-  		login_message
-  		render :new  
-  	end 
+  	if auth
+      @user = User.find_or_create_by_omniauth(auth)
+      session[:user_id] = @user.id 
+
+      redirect_to user_path(@user)
+    else  
+
+    	@user = User.find_by(email: params[:email])
+    	if @user && @user.authenticate(params[:password])
+    		session[:user_id] = @user.id 
+    		redirect_to user_path(@user)
+    	else
+    		login_message
+    		render :new  
+    	end 
+    end 
   end
 
   def destroy
@@ -31,4 +38,8 @@ class SessionsController < ApplicationController
   			flash[:message] = "Please fill out all fields."
   		end 
   	end 
+
+    def auth 
+      request.env['omniauth.auth']
+    end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,12 @@ class User < ApplicationRecord
 		self.workouts.create(workout_attributes)
 		self.save
 	end 
+
+	def self.find_or_create_by_omniauth(auth)
+		where(email: auth[:info][:email]).first_or_create do |user|
+			user.name = auth[:info][:name]
+			user.password = SecureRandom.hex
+		end 
+	end 
+
 end
